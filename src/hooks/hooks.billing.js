f12148f818aa6ea771e4e3cbf326abdb1a97b1f7
Christophe Nouguier <christophe.nouguier@free.fr>
2018-08-27 19:05:48 +0200

    Fixed subscribeDefaultPlan hook

--- src/hooks/hooks.billing.js
+++ src/hooks/hooks.billing.js
@@ -34,9 +34,13 @@ export async function subscribeDefaultPlan (hook) {
   let billingObjectId = hook.result._id
   let billingObjectService = hook.service.path
   debug('Subscribing object ' + billingObjectId + ' of service ' + billingObjectService + ' to default plan')
-  let defaultPlan = _.find(hook.app.get('plans'), { 'default': true })
+  const plans = _.get(hook.app.get('billing'), 'plans')
+  if (_.isNil(plans)) {
+    throw new Error(`The 'subscribeDefaultPlan' requires billing plans to be defined`)
+  }
+  let defaultPlan = _.findKey(plans, { default: true })
   if (_.isNil(defaultPlan)) {
-    throw new Error(`The 'subscribeDefaultPlan' requires a default plan to be defined`)
+    throw new Error(`The 'subscribeDefaultPlan' requires a default billing plan to be defined`)
   }
   const billingService = hook.app.getService('billing')
   await billingService.create({
