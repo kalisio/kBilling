6182741baa8825307311c2dbe7d0f780d155ee4e
Christophe Nouguier <christophe.nouguier@free.fr>
2018-08-30 00:46:28 +0200

    Added id to test components

--- src/client/components/KPlanChooser.vue
+++ src/client/components/KPlanChooser.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <template v-for="(properties, plan) in plans">
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3" :key="plan">
+      <div :id="plan + '-card'" class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3" :key="plan">
         <q-card :color="properties.color" >     
           <q-card-title class="text-center">
             <h4>{{$t('plans.' + plan + '_LABEL', quotas[plan])}}</h4>
@@ -16,15 +16,15 @@
           <q-card-separator />
           <q-card-actions align="end">
             <div v-if="properties.url || properties.route">
-              <q-btn flat @click="onPlanChanged(plan, properties)">{{$t('KPlanChooser.CLICK')}}</q-btn>
+              <q-btn :id="plan+ '-action'" flat @click="onPlanChanged(plan, properties)">{{$t('KPlanChooser.CLICK')}}</q-btn>
             </div>
             <div v-else>
-              <q-btn v-show="plan !== value" flat :disable="properties.subscription && !hasCustomer" @click="onPlanChanged(plan, properties)">{{$t('KPlanChooser.SELECT')}}
-                <q-tooltip v-if="properties.subscription && !hasCustomer">
+              <q-btn :id="plan+ '-action'" v-if="plan !== value" flat :disable="properties.stripeId && !hasCustomer" @click="onPlanChanged(plan, properties)">{{$t('KPlanChooser.SELECT')}}
+                <q-tooltip v-if="properties.stripeId && !hasCustomer">
                   {{$t('KPlanChooser.PLAN_DISABLED_TOOLTIP')}}
                 </q-tooltip>
               </q-btn>
-              <q-btn v-show="plan === value" flat disable>{{$t('KPlanChooser.CURRENT_PLAN')}}</q-btn>
+              <q-btn :id="plan + '-action'" v-else flat disable>{{$t('KPlanChooser.CURRENT_PLAN')}}</q-btn>
             </div>
           </q-card-actions>
         </q-card>
